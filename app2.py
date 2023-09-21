cea173def804e9f37aa15b110e2e3162331f966e
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-21 16:13:31 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,6 +10,10 @@ import sklearn
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
 
 def user_input_features():
     ponp=st.sidebar.slider('Interaction between polymers and nanoparticles: ',0.0,2.5, 0.4)
