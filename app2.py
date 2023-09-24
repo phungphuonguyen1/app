d37d2bdd2de0052e38324f96a43f45d7b1cca1b3
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-24 21:20:16 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -104,30 +104,6 @@ st.write("For more information, please read this article:  [nanoNET: machine lea
 custom_button_style = """
     <style>
     .my-button {
-        background-color: #219C90; /* Green background color */
-        color: #FFFFFF; /* White text color */
-        border-radius: 20px; /* Rounded corners */
-        padding: 10px 20px; /* Padding around the text */
-        font-weight: bold; /* Bold text */
-        cursor: pointer; /* Add a pointer cursor on hover */
-    }
-    .my-button:hover {
-        background-color: #009900; /* Change background color on hover */
-    }
-    </style>
-"""
-
-# Display the custom-styled button
-st.markdown(custom_button_style, unsafe_allow_html=True)
-if st.button("Styled Button", key="my_button"):
-    st.success("Button Clicked!")
-
-import streamlit as st
-
-# Define a custom CSS style
-custom_button_style = """
-    <style>
-    .my-button {
         background-color: orange;
         color: white;
         padding: 10px 20px;
