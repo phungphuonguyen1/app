91d47bde5b4f24b48e3e354f0ae3f11fbf8202c1
phungphuonguyen1 <114821616+phungphuonguyen1@users.noreply.github.com>
2023-09-25 08:48:50 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -147,8 +147,10 @@ if st.sidebar.button("Predict!"):
     data_res=pd.DataFrame(res)
     #st.write(type(data_res))
     #st.write(data_res)
-    frame1=data_res.iloc[:,6]
-    frame2=data_res.iloc[:,7]
+    frame1=data_res.iloc[:,6].values
+    frame2=data_res.iloc[:,7].values
+    st.write('min x: ',min(frame1))
+    st.write('max x: ',max(frame1))
     st.write(frame1)
     st.write(frame2)
     st.subheader('Prediction')
